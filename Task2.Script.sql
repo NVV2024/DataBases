e973ac0000b690e78baad34099db381c5f6f6637
NVV2024 <vadn@yandex.ru>
2024-07-20 18:28:52 +0300

    Task2. final Script.sql

--- Task2.Script.sql
+++ Task2.Script.sql
@@ -1,13 +1,48 @@
-CREATE TABLE musitian (musitian_id serial PRIMARY KEY, Name VARCHAR(100));
+CREATE TABLE musitian (
+      id serial PRIMARY KEY, 
+      Name VARCHAR(100)
+  );
 
-CREATE TABLE album (albom_id SERIAL PRIMARY KEY, Name VARCHAR(100), Year DATA, musitian_id NOT NULL REFERNCE musitian(musitian_id));
+CREATE TABLE albom (
+        id SERIAL PRIMARY KEY, 
+        Name VARCHAR(100), 
+        Year DATA, 
+        musitian_id NOT NULL REFERNCE musitian(id)
+);
 
-                                                           ^
-CREATE TABLE album (albom_id SERIAL PRIMARY KEY, Name VARCHAR(100), year DATE, musitian_id INTEGER NOT NULL REFERENCES musitian(musitian_id));
+CREATE TABLE musitianalbom (
+        id SERIAL PRIMARY KEY, 
+        musitian_id INTEGER NOT NULL REFERENCES musitian(id),
+        albom_id INTEGER NOT NULL REFERENCES albom(id)
+      );
 
+CREATE TABLE track (
+        id SERIAL PRIMARY KEY, 
+        Name VARCHAR(100), 
+        duration TIME, 
+        albom_id INTEGER NOT NULL REFERENCES albom(id)
+  );
 
-CREATE TABLE track (track_id SERIAL PRIMARY KEY, Name VARCHAR(100), duration TIME, albom_id INTEGER NOT NULL REFERENCES album(albom_id));
+CREATE TABLE collection (
+      id SERIAL PRIMARY KEY, 
+      Name VARCHAR(100), 
+      year DATE
+  );
 
-CREATE TABLE collection (colection_id SERIAL PRIMARY KEY, Name VARCHAR(100), year DATE);
+CREATE TABLE CollectionTracks (
+        id SERIAL PRIMARY KEY, 
+        colection_id INTEGER NOT NULL REFERENCES collection(id), 
+        track_id INTEGER NOT NULL REFERENCES track(id)
+  );
 
-CREATE TABLE CollectionTracks (id SERIAL PRIMARY KEY, colection_id INTEGER NOT NULL REFERENCES collection(colection_id), track_id INTEGER NOT NULL REFERENCES track(track_id));
+CREATE TABLE AlbomCollection (
+		id SERIAL PRIMARY key,
+		albom_id INTEGER NOT NULL REFERENCES albom(id),
+		collection_id INTEGER NOT NULL REFERENCES collection(id)
+);
+
+CREATE TABLE TrackList (
+		TrackName INTEGER REFERENCES track(id),
+		AlbomName INTEGER REFERENCES albom(id),
+		CONSTRAINT pk PRIMARY KEY (TrackName, AlbomName) 
+);
